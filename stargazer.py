9bde49d1b9fe17f8fbe0cd8989c367a4c0984d9a
Spencer Todd <cheesy.mac.dott@gmail.com>
2022-03-29 14:56:21 -0400

    Update stargazers rpi server

--- stargazer.py
+++ stargazer.py
@@ -2,11 +2,12 @@
 # on the rpi
 import struct
 import serial
+import time
 from skyfield.api import load
 from skyfield.api import N, E, wgs84
 
-planets = load('de421.bsp')
 
+planets = load('de421.bsp')
 
 def get_star_location(star: str, lat: float, lon: float):
     ts = load.timescale()
@@ -20,15 +21,44 @@ def get_star_location(star: str, lat: float, lon: float):
 
 
 def handle_requests():
-    ser = serial.Serial('/dev/ttyS1')
+    #ser = serial.Serial('/dev/ttyS1')
+    ser = serial.Serial('COM33', 115200, timeout=999)
 
     while True:
-        request = ser.readline().decode().split()
+        #print("before readline")
+        request = ser.readline()
+
+        if not request:
+            time.sleep(0.1)
+            continue
+
+        print(request)
+        request = request.decode().split()
 
         if request[0] == 'S':
-            _, star, lat, lon = request
-            alt, az = get_star_location(star, float(lat), float(lon))
+            _, star, lat, lon, *extra = request
+
+            if extra:
+                print("extra S args:", *extra)
+                continue
+
+            try:
+                lat, lon = float(lat), float(lon)
+            except ValueError:
+                print("malformed args:", lat, lon)
+                continue
+
+            alt, az = get_star_location(star, lat, lon)
+            print("computer calculated:", alt, az)
+            time.sleep(.05)
             ser.write(struct.pack('<ff', alt, az))
         elif request[0] == 'P':
             # take picture
+            print("TODO:  print request")
             ...
+        else:
+            print("log:", *request)
+            
+        time.sleep(.5)
+
+handle_requests()
