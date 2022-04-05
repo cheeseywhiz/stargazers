381f54e408d0c28036c0a84b2b8cfc0bf2d91ea2
Spencer Todd <cheesy.mac.dott@gmail.com>
2022-04-05 14:41:39 -0400

    Get star location, image, and gps transfer working

--- main.c
+++ main.c
@@ -43,8 +43,7 @@ UART_HandleTypeDef hlpuart1;
 UART_HandleTypeDef huart3;
 
 /* USER CODE BEGIN PV */
-static const int DATA_SIZE = 100;
-uint8_t testData[100] = "Not Yet"; // DATA_SIZE
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -58,6 +57,118 @@ static void MX_USART3_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+/***GPS Code***/
+static const int DATA_SIZE = 100;
+/*
+//uint8_t testData[DATA_SIZE];
+typedef struct {
+	int hour;
+	int min;
+	double sec;
+	double lat;
+	char NS;
+	double lon;
+	char EW;
+}gpsStruct;
+
+//struct list GPSInfo;
+
+int getGPSInfo(uint8_t info[DATA_SIZE], gpsStruct GPSInfo){
+	uint8_t comma = ',';
+	// Find $GPGGA first
+	int offsetIndex = 0;
+	while(info[offsetIndex] != '$' || info[offsetIndex + 1] != 'G' || info[offsetIndex + 2] != 'P' || info[offsetIndex + 3] != 'G' || info[offsetIndex + 4] != 'G' || info[offsetIndex + 5] != 'A'){
+		++offsetIndex;
+		if(offsetIndex >= DATA_SIZE - 45){
+			return -1;
+		}
+	}
+	// Get the time (first comma at index 6 last one at 17)
+	int indexTime = 7 + offsetIndex;
+	GPSInfo.hour = (int) info[indexTime];
+	GPSInfo.hour = GPSInfo.hour * 10;
+	GPSInfo.hour = GPSInfo.hour + ((int) info[indexTime + 1]);
+
+	GPSInfo.min = (int) info[indexTime + 2];
+	GPSInfo.min = GPSInfo.min * 10;
+	GPSInfo.min = GPSInfo.min + ((int) info[indexTime + 3]);
+
+	GPSInfo.sec = (10 * ((int) info[indexTime + 5])) + info[indexTime + 6] + (0.1 * ((int) info[indexTime + 7])) + (0.01 * ((int) info[indexTime + 8])) + (0.001 * ((int) info[indexTime + 9]));
+
+	// Get Latitude if available (first comma at index 17)
+	int iLat = 18 + offsetIndex;
+	// If GPS does not register any coordinates
+	if(info[iLat] == comma){
+		return -2;
+	}
+	GPSInfo.lat = (1000 * ((double) info[iLat])) + (100 * ((double) info[iLat+1])) + (10 * ((double) info[iLat+2])) + (double) info[iLat+3] + (0.1 * ((double) info[iLat+5])) + (0.01 * ((double) info[iLat+6])) + (0.001 * ((double) info[iLat+7])) + (0.0001 * ((double) info[iLat+8]));
+
+	// N/S Indicator
+	GPSInfo.NS = (char) info[iLat + 10];
+
+	// Longitude
+	int iLon = 30;
+	GPSInfo.lon = (1000 * ((double) info[iLon])) + (100 * ((double) info[iLon+1])) + (10 * ((double) info[iLon+2])) + (double) info[iLon+3] + (0.1 * ((double) info[iLon+5])) + (0.01 * ((double) info[iLon+6])) + (0.001 * ((double) info[iLon+7])) + (0.0001 * ((double) info[iLon+8]));
+
+	// E/W Indicator
+	GPSInfo.EW = (char) info[iLon+10];
+
+
+	return 0;
+}
+*/
+
+const char* getGPSString(uint8_t buffer[DATA_SIZE]) {
+	char *gps_string = (void*)buffer;
+	while(gps_string[0] != '$') {
+		gps_string++;
+	}
+	// now gps_string starts with $
+	uint16_t length = 0;
+	while (gps_string[length] != '\n') {
+		length++;
+	}
+	// now length points to \n
+	// overwrite CRLF with NL
+	gps_string[length - 1] = '\0';
+	return gps_string;
+}
+/***End of GPS Code***/
+
+struct pixel {
+	uint8_t red, green, blue;
+};
+
+#define IMAGE_HEIGHT 320
+#define IMAGE_WIDTH 480
+
+struct image {
+	struct pixel image[IMAGE_WIDTH][IMAGE_HEIGHT];
+};
+
+struct image image;
+
+void uart_receive(void *buffer, size_t size) {
+	size_t transferred = 0;
+
+	while (transferred < size) {
+		// num bytes left to transfer
+		size_t n_left = size - transferred;
+		// size of the next transfer
+		uint16_t transfer_size = n_left <= 0xffff ? n_left : 0xffff;
+		HAL_UART_Receive(&hlpuart1, buffer, transfer_size, 0xFFFF);
+		buffer = (void*)((uintptr_t)buffer + transfer_size);
+		transferred += transfer_size;
+		/*
+		struct pixel *pixels = (struct pixel*)buffer;
+		struct pixel pixel0 = pixels[0];
+		struct pixel pixel1 = pixels[1];
+		struct pixel pixel2 = pixels[2];
+		int z = 0;
+		*/
+	}
+}
+
 struct star_location {
     float altitude;
     float azimuth;
@@ -66,10 +177,14 @@ struct star_location {
 struct star_location get_star_location(const char *star, float lat, float lon) {
 	printf("S %s %f %f\n", star, lat, lon);
     struct star_location location;
-    HAL_UART_Receive(&hlpuart1, (void*)&location, sizeof(location), 0xFFFF);
+    uart_receive((void*)&location, sizeof(location));
     return location;
 }
 
+void capture_image(void) {
+	printf("P\n");
+	uart_receive((void*)&image, sizeof(image));
+}
 /* USER CODE END 0 */
 
 /**
@@ -103,31 +218,38 @@ int main(void)
   MX_LPUART1_UART_Init();
   MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
-  uint8_t testDataDisplay[100] = "Another"; // DATA_SIZE
-  int success = 1;
+  uint8_t gps_buffer[DATA_SIZE];
+  //gpsStruct GPSInfo;
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	struct star_location location = get_star_location("mars", 42.3583, -71.0636);
-	printf("mars location: alt: %f \tazi: %f\n", location.altitude, location.azimuth);
-	int x = 0;
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	HAL_StatusTypeDef GPSStatus = HAL_UART_Receive(&huart3, (uint8_t*) gps_buffer, 100, 5000);
+	const char *gps_string = getGPSString(gps_buffer);
+	printf("%s\n", gps_string);
+//	for(int i = 0; i < 100; ++i){
+//		testDataDisplay[i] = testData[i];
+//	}
+//	getGPSInfo(testData, GPSInfo);
 
-	HAL_StatusTypeDef t = HAL_UART_Receive(&huart3, (uint8_t*) testData, 100, 5000); // DATA_SIZE
-	if(t != HAL_OK){
-		success = 0;
+	// TODO: change get_star_location to send the gps string with the star name appended to the end (with a comma ofc)
+
+	for (int i = 0; i < 10; i++) {
+		struct star_location location = get_star_location("mars", 42.3583, -71.0636);
+		printf("mars location: alt: %f \tazi: %f\n", location.altitude, location.azimuth);
+		int x = 0;
 	}
-	else{
-		success = 1;
-	}
-	for(int i = 0; i < DATA_SIZE; ++i){
-		testDataDisplay[i] = testData[i];
-	}
+
+	capture_image();
+	struct pixel first = image.image[0][0], last = image.image[319][479];
+	printf("pixel (0, 0): r: %d\tg: %d\tb: %d\n", first.red, first.green, first.blue);
+	printf("pixel (319, 479): r: %d\tg: %d\t: b: %d\n", last.red, last.green, last.blue);
+	int y = 0;
   }
   /* USER CODE END 3 */
 }
@@ -195,11 +317,11 @@ static void MX_LPUART1_UART_Init(void)
   hlpuart1.Init.StopBits = UART_STOPBITS_1;
   hlpuart1.Init.Parity = UART_PARITY_NONE;
   hlpuart1.Init.Mode = UART_MODE_TX_RX;
-  hlpuart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  hlpuart1.Init.HwFlowCtl = UART_HWCONTROL_RTS_CTS;
   hlpuart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
   hlpuart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
   hlpuart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  hlpuart1.FifoMode = UART_FIFOMODE_DISABLE;
+  hlpuart1.FifoMode = UART_FIFOMODE_ENABLE;
   if (HAL_UART_Init(&hlpuart1) != HAL_OK)
   {
     Error_Handler();
@@ -212,7 +334,7 @@ static void MX_LPUART1_UART_Init(void)
   {
     Error_Handler();
   }
-  if (HAL_UARTEx_DisableFifoMode(&hlpuart1) != HAL_OK)
+  if (HAL_UARTEx_EnableFifoMode(&hlpuart1) != HAL_OK)
   {
     Error_Handler();
   }
@@ -260,7 +382,7 @@ static void MX_USART3_UART_Init(void)
   {
     Error_Handler();
   }
-  if (HAL_UARTEx_DisableFifoMode(&huart3) != HAL_OK)
+  if (HAL_UARTEx_EnableFifoMode(&huart3) != HAL_OK)
   {
     Error_Handler();
   }
@@ -287,8 +409,6 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOG_CLK_ENABLE();
-  HAL_PWREx_EnableVddIO2();
 
   /*Configure GPIO pins : PE2 PE3 */
   GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
@@ -314,10 +434,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Alternate = GPIO_AF13_SAI1;
   HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PC0 PC1 PC2 PC3
-                           PC4 PC5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                          |GPIO_PIN_4|GPIO_PIN_5;
+  /*Configure GPIO pins : PC2 PC3 PC4 PC5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG_ADC_CONTROL;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
@@ -330,14 +448,14 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PA1 PA3 */
-  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_3;
+  /*Configure GPIO pin : PA3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG_ADC_CONTROL;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PA4 PA5 PA6 PA7 */
-  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
+  /*Configure GPIO pins : PA4 PA5 PA7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_7;
   GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
@@ -350,12 +468,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PB1 */
-  GPIO_InitStruct.Pin = GPIO_PIN_1;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG_ADC_CONTROL;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PB2 PB6 */
@@ -381,14 +493,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   GPIO_InitStruct.Alternate = GPIO_AF3_TIM1_COMP1;
   HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PB10 */
-  GPIO_InitStruct.Pin = GPIO_PIN_10;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PB12 PB13 PB15 */
   GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_15;
